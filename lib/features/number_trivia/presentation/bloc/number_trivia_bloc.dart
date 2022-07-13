9413ce77add5dd152bf6f680427bb0ac22c1ae99
LPUSZ <pusz.lukasz@gmail.com>
2022-07-13 21:26:35 +0200

    Flutter TDD Clean Architecture Course [12] – Bloc Implementation 2/2

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,5 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:number_trivia/core/error/failures.dart';
+import 'package:number_trivia/core/use_cases/use_case.dart';
 import 'package:number_trivia/core/util/input_converter.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart';
@@ -25,21 +27,75 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
     required this.converter,
   }) : super(Empty()) {
     on<GetTriviaForConcreteNumber>(_onGetTriviaForConcreteNumber);
+    on<GetTriviaForRandomNumber>(_onGetTriviaForRandomNumber);
   }
 
   void _onGetTriviaForConcreteNumber(
     GetTriviaForConcreteNumber event,
     Emitter<NumberTriviaState> emit,
-  ) {
+  ) async {
     final inputEither = converter.stringToUnsignedInteger(event.numberString);
     inputEither.fold(
+      (Failure failure) => _emitInvalidInput(emit),
+      (int number) async => await _tryDownloadTrivia(emit, number),
+    );
+  }
+
+  void _onGetTriviaForRandomNumber(
+    GetTriviaForRandomNumber event,
+    Emitter<NumberTriviaState> emit,
+  ) async {
+    emit(Loading());
+
+    final failureOrTrivia = await getRandom(NoParams());
+    failureOrTrivia.fold(
       (failure) {
-        final error = Error(message: invalidInputFailureMessage);
-        emit(error);
+        final errorState = Error(
+          message: _mapFailureToMessage(failure),
+        );
+        emit(errorState);
       },
-      (integer) {
-        // throw UnimplementedError();
+      (trivia) {
+        emit(Loaded(trivia: trivia));
       },
     );
+  }
+
+  void _emitInvalidInput(Emitter<NumberTriviaState> emit) {
+    final error = Error(message: invalidInputFailureMessage);
+    emit(error);
+  }
+
+  Future<void> _tryDownloadTrivia(
+    Emitter<NumberTriviaState> emit,
+    int number,
+  ) async {
+    emit(Loading());
+
+    final params = Params(number: number);
+    final failureOrTrivia = await getConcrete(params);
+    failureOrTrivia.fold(
+      (Failure failure) {
+        final errorState = Error(
+          message: _mapFailureToMessage(failure),
+        );
+        emit(errorState);
+      },
+      (NumberTrivia trivia) {
+        final loadedState = Loaded(trivia: trivia);
+        emit(loadedState);
+      },
+    );
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return serverFailureMessage;
+      case CacheFailure:
+        return cacheFailureMessage;
+      default:
+        return 'Unexpected Error';
+    }
   }
 }
--- test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
+++ test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
@@ -3,6 +3,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:number_trivia/core/error/failures.dart';
 import 'package:number_trivia/core/util/input_converter.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart';
@@ -46,48 +47,278 @@ void main() {
     expect(bloc.state, Empty());
   });
 
-  blocTest<NumberTriviaBloc, NumberTriviaState>(
-    'should call the input converter to validate '
-    'and convert the string on an unsigned integer',
-    setUp: () {
-      when(converter.stringToUnsignedInteger(any)).thenReturn(
-        const Right(1),
-      );
-    },
-    build: () => NumberTriviaBloc(
-      getRandom: getRandom,
-      getConcrete: getConcrete,
-      converter: converter,
-    ),
-    act: (bloc) => bloc.add(
-      GetTriviaForConcreteNumber('1'),
-    ),
-    verify: (_) {
-      verify(converter.stringToUnsignedInteger('1'));
-    },
-  );
+  group('get concrete trivia tests', () {
+    blocTest<NumberTriviaBloc, NumberTriviaState>(
+      'should call the input converter to validate '
+      'and convert the string on an unsigned integer',
+      setUp: () {
+        when(converter.stringToUnsignedInteger(any)).thenReturn(
+          const Right(1),
+        );
 
-  blocTest<NumberTriviaBloc, NumberTriviaState>(
-    'should emit [Error] when the input is invalid',
-    setUp: () {
-      when(converter.stringToUnsignedInteger(any)).thenReturn(
-        Left(InvalidInputFailure()),
-      );
-    },
-    build: () => NumberTriviaBloc(
-      getRandom: getRandom,
-      getConcrete: getConcrete,
-      converter: converter,
-    ),
-    act: (bloc) => bloc.add(
-      GetTriviaForConcreteNumber('1'),
-    ),
-    expect: () => [
-      isA<Error>().having(
-        (state) => state.message,
-        'should have input failure message',
-        invalidInputFailureMessage,
-      )
-    ],
-  );
+        when(getConcrete(any)).thenAnswer(
+          (_) async => Right(expectedTrivia),
+        );
+      },
+      build: () => NumberTriviaBloc(
+        getRandom: getRandom,
+        getConcrete: getConcrete,
+        converter: converter,
+      ),
+      act: (bloc) => bloc.add(
+        GetTriviaForConcreteNumber('1'),
+      ),
+      verify: (_) {
+        verify(converter.stringToUnsignedInteger('1'));
+      },
+    );
+
+    blocTest<NumberTriviaBloc, NumberTriviaState>(
+      'should emit [Error] when the input is invalid',
+      setUp: () {
+        when(converter.stringToUnsignedInteger(any)).thenReturn(
+          Left(InvalidInputFailure()),
+        );
+      },
+      build: () => NumberTriviaBloc(
+        getRandom: getRandom,
+        getConcrete: getConcrete,
+        converter: converter,
+      ),
+      act: (bloc) => bloc.add(
+        GetTriviaForConcreteNumber('1'),
+      ),
+      expect: () => [
+        isA<Error>().having(
+          (state) => state.message,
+          'should have input failure message',
+          invalidInputFailureMessage,
+        )
+      ],
+    );
+
+    blocTest<NumberTriviaBloc, NumberTriviaState>(
+      'should get data from concrete use case',
+      setUp: () {
+        when(converter.stringToUnsignedInteger(any)).thenReturn(
+          const Right(1),
+        );
+        when(getConcrete(any)).thenAnswer(
+          (_) async => Right(expectedTrivia),
+        );
+      },
+      build: () => NumberTriviaBloc(
+        getRandom: getRandom,
+        getConcrete: getConcrete,
+        converter: converter,
+      ),
+      act: (bloc) => bloc.add(
+        GetTriviaForConcreteNumber('1'),
+      ),
+      verify: (_) {
+        verify(getConcrete(const Params(number: 1)));
+      },
+    );
+
+    blocTest<NumberTriviaBloc, NumberTriviaState>(
+      'should emit [Loading, Loaded] when data is gotten',
+      setUp: () {
+        when(converter.stringToUnsignedInteger(any)).thenReturn(
+          const Right(1),
+        );
+        when(getConcrete(any)).thenAnswer(
+          (_) async => Right(expectedTrivia),
+        );
+      },
+      build: () => NumberTriviaBloc(
+        getRandom: getRandom,
+        getConcrete: getConcrete,
+        converter: converter,
+      ),
+      act: (bloc) => bloc.add(
+        GetTriviaForConcreteNumber('1'),
+      ),
+      verify: (_) {
+        verify(getConcrete(const Params(number: 1)));
+      },
+      expect: () => [
+        isA<Loading>(),
+        isA<Loaded>().having(
+          (state) => state.trivia,
+          'should have correct trivia',
+          expectedTrivia,
+        ),
+      ],
+    );
+
+    blocTest<NumberTriviaBloc, NumberTriviaState>(
+      'should emit [Loading, Error] when getting data fails',
+      setUp: () {
+        when(converter.stringToUnsignedInteger(any)).thenReturn(
+          const Right(1),
+        );
+        when(getConcrete(any)).thenAnswer(
+          (_) async => Left(ServerFailure()),
+        );
+      },
+      build: () => NumberTriviaBloc(
+        getRandom: getRandom,
+        getConcrete: getConcrete,
+        converter: converter,
+      ),
+      act: (bloc) => bloc.add(
+        GetTriviaForConcreteNumber('1'),
+      ),
+      verify: (_) {
+        verify(getConcrete(const Params(number: 1)));
+      },
+      expect: () => [
+        isA<Loading>(),
+        isA<Error>().having(
+          (state) => state.message,
+          'should have serverFailureMessage message',
+          serverFailureMessage,
+        ),
+      ],
+    );
+
+    blocTest<NumberTriviaBloc, NumberTriviaState>(
+      'should emit [Loading, Error] with a proper message for the error'
+      'when getting data fails',
+      setUp: () {
+        when(converter.stringToUnsignedInteger(any)).thenReturn(
+          const Right(1),
+        );
+        when(getConcrete(any)).thenAnswer(
+          (_) async => Left(CacheFailure()),
+        );
+      },
+      build: () => NumberTriviaBloc(
+        getRandom: getRandom,
+        getConcrete: getConcrete,
+        converter: converter,
+      ),
+      act: (bloc) => bloc.add(
+        GetTriviaForConcreteNumber('1'),
+      ),
+      verify: (_) {
+        verify(getConcrete(const Params(number: 1)));
+      },
+      expect: () => [
+        isA<Loading>(),
+        isA<Error>().having(
+          (state) => state.message,
+          'should have cacheFailureMessage message',
+          cacheFailureMessage,
+        ),
+      ],
+    );
+  });
+
+  group('get random trivia tests', () {
+    blocTest<NumberTriviaBloc, NumberTriviaState>(
+      'should get data from concrete use case',
+      setUp: () {
+        when(getRandom(any)).thenAnswer(
+          (_) async => Right(expectedTrivia),
+        );
+      },
+      build: () => NumberTriviaBloc(
+        getRandom: getRandom,
+        getConcrete: getConcrete,
+        converter: converter,
+      ),
+      act: (bloc) => bloc.add(
+        GetTriviaForRandomNumber(),
+      ),
+      verify: (_) {
+        verify(getRandom(any));
+      },
+    );
+
+    blocTest<NumberTriviaBloc, NumberTriviaState>(
+      'should emit [Loading, Loaded] when data is gotten',
+      setUp: () {
+        when(getRandom(any)).thenAnswer(
+          (_) async => Right(expectedTrivia),
+        );
+      },
+      build: () => NumberTriviaBloc(
+        getRandom: getRandom,
+        getConcrete: getConcrete,
+        converter: converter,
+      ),
+      act: (bloc) => bloc.add(
+        GetTriviaForRandomNumber(),
+      ),
+      verify: (_) {
+        verify(getRandom(any));
+      },
+      expect: () => [
+        isA<Loading>(),
+        isA<Loaded>().having(
+          (state) => state.trivia,
+          'should have correct trivia',
+          expectedTrivia,
+        ),
+      ],
+    );
+
+    blocTest<NumberTriviaBloc, NumberTriviaState>(
+      'should emit [Loading, Error] when getting data fails',
+      setUp: () {
+        when(getRandom(any)).thenAnswer(
+          (_) async => Left(ServerFailure()),
+        );
+      },
+      build: () => NumberTriviaBloc(
+        getRandom: getRandom,
+        getConcrete: getConcrete,
+        converter: converter,
+      ),
+      act: (bloc) => bloc.add(
+        GetTriviaForRandomNumber(),
+      ),
+      verify: (_) {
+        verify(getRandom(any));
+      },
+      expect: () => [
+        isA<Loading>(),
+        isA<Error>().having(
+          (state) => state.message,
+          'should have serverFailureMessage message',
+          serverFailureMessage,
+        ),
+      ],
+    );
+
+    blocTest<NumberTriviaBloc, NumberTriviaState>(
+      'should emit [Loading, Error] with a proper message for the error'
+      'when getting data fails',
+      setUp: () {
+        when(getRandom(any)).thenAnswer(
+          (_) async => Left(CacheFailure()),
+        );
+      },
+      build: () => NumberTriviaBloc(
+        getRandom: getRandom,
+        getConcrete: getConcrete,
+        converter: converter,
+      ),
+      act: (bloc) => bloc.add(
+        GetTriviaForRandomNumber(),
+      ),
+      verify: (_) {
+        verify(getRandom(any));
+      },
+      expect: () => [
+        isA<Loading>(),
+        isA<Error>().having(
+          (state) => state.message,
+          'should have cacheFailureMessage message',
+          cacheFailureMessage,
+        ),
+      ],
+    );
+  });
 }
